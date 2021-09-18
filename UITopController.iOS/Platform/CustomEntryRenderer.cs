8ac1aeda212ee372b1248126bbe4b8103c1b14d4
Thaveesha Kannangara <thaveesheka@gmail.com>
2021-09-18 11:54:44 +0530

    First Commit

---
+++ UITopController.iOS/Platform/CustomEntryRenderer.cs
@@ -0,0 +1,42 @@
+﻿using CoreAnimation;
+using CoreGraphics;
+using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using UITopController.CustomRenderers;
+using UITopController.iOS.Platform;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+namespace UITopController.iOS.Platform
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        private CALayer _line;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            _line = null;
+
+            if (Control == null || e.NewElement == null)
+                return;
+
+            Control.BorderStyle = UITextBorderStyle.None;
+
+            _line = new CALayer
+            {
+                BorderColor = UIColor.FromRGB(174, 174, 174).CGColor,
+                BackgroundColor = UIColor.FromRGB(174, 174, 174).CGColor,
+                Frame = new CGRect(0, Frame.Height / 2, Frame.Width * 2, 1f)
+            };
+
+            Control.Layer.AddSublayer(_line);
+        }
+
+    }
+}
---
+++ UITopController/CustomRenderers/CustomEntry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace UITopController.CustomRenderers
+{
+    public class CustomEntry : Entry
+    {
+    }
+}
